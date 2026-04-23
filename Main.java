48c026c109f90e5a475523a47d3d6f3dd325bfe5
egli3 <hilajegli@gmail.com>
2026-04-23 13:48:58 -0400

    skins

--- Main.java
+++ Main.java
@@ -4,16 +4,13 @@ import java.awt.*;
 public class Main{
 
     Swing.Utilities.invokeLater(() -> {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("My Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null);
-        frame.setTitle("CS2 Case Simulator");
-        
-        
+        frame.setLocationRelativeTo(null); // Center the frame on the screen
         frame.setVisible(true);
 
-
+        
     });
 
 

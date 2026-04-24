de4faba646e7bcd54a1a466befa01a3829247116
Aron <aron.kubat@gmail.com>
2026-04-24 14:55:26 -0400

    clean up

--- src/CaseItem.java
+++ src/CaseItem.java
@@ -6,13 +6,16 @@ public class CaseItem {
     private String name;
     private ItemRarity rarity;
     private float wear;
+    private int patternIndex;
     private Image icon;
 
-    public CaseItem(String name, ItemRarity rarity, float wear, Image icon) {
+
+    public CaseItem(String name, ItemRarity rarity, float wear, Image icon, int patternIndex) {
         this.name = name;
         this.rarity = rarity;
         this.wear = wear;
         this.icon = icon;
+        this.patternIndex = patternIndex;
     }
     
 }

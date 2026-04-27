8c617da54f922861fd6c1e59165979b7656511f9
Aron <aron.kubat@gmail.com>
2026-04-27 16:48:55 -0400

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,35 @@
 package src;
+import java.awt.Color;
 import java.net.URL;
 import javax.swing.*;
 
 public class Main{
 
+    public Main(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080);
+        frame.setResizable(true);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("CS2 Case Simulator");
+        frame.getContentPane().setBackground(new Color(10, 9, 13));
+
+        URL iconURL = Main.class.getResource("/SourceImages/main-icon.png");
+        if (iconURL != null) {
+            ImageIcon icon = new ImageIcon(iconURL);
+            frame.setIconImage(icon.getImage());
+        } else {
+            System.err.println("Icon image not found at: " + iconURL);
+        }
+
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(1920, 1080);
-            frame.setResizable(true);
-            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-            frame.setLocationRelativeTo(null);
-            frame.setTitle("CS2 Case Simulator");
-
-            URL iconURL = Main.class.getResource("/SourceImages/main-icon.png");
-            if (iconURL != null) {
-                ImageIcon icon = new ImageIcon(iconURL);
-                frame.setIconImage(icon.getImage());
-            } else {
-                System.err.println("Icon image not found at: " + iconURL);
-            }
-
-            frame.setVisible(true);
+            Main main = new Main();
         });
 
 

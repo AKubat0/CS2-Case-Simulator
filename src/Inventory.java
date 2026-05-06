6c85d7b25feae031252479317da1953eaa0fc7d6
Aron <aron.kubat@gmail.com>
2026-05-06 14:49:32 -0400

    start adding file saving inventory

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,12 +1,25 @@
 package src;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Inventory {
+
+    File inventoryFile = new File("inventory.csv");
     
     private ArrayList<CaseItem> items;
 
     public Inventory() {
         this.items = new ArrayList<>();
+
+        if (!inventoryFile.exists()){
+            try {
+                inventoryFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            loadInventory();
+        }
     }
 
     public void addItem(CaseItem item) {
@@ -16,4 +29,21 @@ public class Inventory {
     public ArrayList<CaseItem> getItems() {
         return items;
     }
+
+    public void saveInventory(){
+
+        try(FileWriter fw = new FileWriter(inventoryFile, true)){
+            for (CaseItem item : items) {
+                fw.write(item.getName() + "," + item.getRarity() + "," + item.getIcon() + "," + item.getWear() + "," + item.getPatternIndex() + "," + item.isStatTrak() + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadInventory(){
+
+    }
+
+
 }
